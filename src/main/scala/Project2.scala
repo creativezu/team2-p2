7c7945ce3d7c4dce4bd89cc79aa54fa85126b370
creativezu <chris.nzuriwatu@gmail.com>
2022-02-09 12:22:24 -0500

    Adding sbt, csv, project and target

--- src/main/scala/Project2.scala
+++ src/main/scala/Project2.scala
@@ -26,8 +26,8 @@ object Project2 {
 
     
 
-    // Run method to insert Covid data. Only needs to be ran initially, then table data1 will be persisted.
-    insertCovidData(hiveCtx)
+    // Run method to insert Covid data. Only needs to be ran initially, then table data1A will be persisted.
+    //insertCovidData(hiveCtx)
 
     /*
      * Here is where I would ask the user for input on what queries they would like to run, as well as
@@ -35,7 +35,7 @@ object Project2 {
      *
      */
 
-    top10DeathRates(hiveCtx)
+    //top10DeathRates(hiveCtx)
 
     sc.stop() // Necessary to close cleanly. Otherwise, spark will continue to run and run into problems.
   }
@@ -43,8 +43,8 @@ object Project2 {
   
 
   def insertCovidData(hiveCtx: HiveContext): Unit = {
-    //hiveCtx.sql("LOAD DATA LOCAL INPATH 'input/covid_19_data.txt' OVERWRITE INTO TABLE data1")
-    //hiveCtx.sql("INSERT INTO data1 VALUES (1, 'date', 'California', 'US', 'update', 10, 1, 0)")
+    //hiveCtx.sql("LOAD DATA LOCAL INPATH 'input/covid_19_data.txt' OVERWRITE INTO TABLE data1A")
+    //hiveCtx.sql("INSERT INTO data1A VALUES (1, 'date', 'California', 'US', 'update', 10, 1, 0)")
 
     // This statement creates a DataFrameReader from your file that you wish to pass in. We can infer the schema and retrieve
     // column names if the first row in your csv file has the column names. If not wanted, remove those options. This can
@@ -61,21 +61,21 @@ object Project2 {
     // These next three lines will create a temp view from the dataframe you created and load the data into a permanent table inside
     // of Hadoop. Thus, we will have data persistence, and this code only needs to be ran once. Then, after the initializatio, this
     // code as well as the creation of output will not be necessary.
-    output.createOrReplaceTempView("temp_data")
-    hiveCtx.sql(
-      "CREATE TABLE IF NOT EXISTS data1 (SNo INT, ObservationDate STRING, Province_State STRING, Country_Region STRING, LastUpdate STRING, Confirmed INT, Deaths INT, Recovered INT)"
-    )
-    hiveCtx.sql("INSERT INTO data1 SELECT * FROM temp_data")
+    // output.createOrReplaceTempView("temp_data")
+    // hiveCtx.sql(
+    //   "CREATE TABLE IF NOT EXISTS data1A (iso_code INT, continent STRING, location STRING, date STRING, total_cases INT, new_cases INT, total_deaths INT, new_deaths INT, total_tests INT, total_vaccinations INT, people_vaccinated INT, people_fully_vaccinated INT, population INT, population_density INT, median_age INT, aged_65_older INT, aged_70_older INT, gdp_per_capita INT, hospital_beds_per_thousand INT, life_expectancy INT)"
+    // )
+    // hiveCtx.sql("INSERT INTO data1A SELECT * FROM temp_data")
 
-    // To query the data1 table. When we make a query, the result set ius stored using a dataframe. In order to print to the console,
+    // To query the data1A table. When we make a query, the result set ius stored using a dataframe. In order to print to the console,
     // we can use the .show() method.
-    val summary = hiveCtx.sql("SELECT * FROM data1 LIMIT 10")
-    summary.show()
+    // val summary = hiveCtx.sql("SELECT * FROM data1A LIMIT 10")
+    // summary.show()
   }
 
   def top10DeathRates(hiveCtx: HiveContext): Unit = {
     val result = hiveCtx.sql(
-      "SELECT Province_State State, MAX(deaths)/MAX(confirmed) Death_Rate FROM data1 WHERE country_region='US' AND Province_State NOT LIKE '%,%' GROUP BY State ORDER BY Death_Rate DESC LIMIT 10"
+      "SELECT continent, MAX(total_cases)/MAX(total_deaths) Death_Rate FROM data1A WHERE continent='Asia' GROUP BY continent ORDER BY Death_Rate DESC LIMIT 10"
     )
     result.show()
     result.write.csv("results/top10DeathRates")
