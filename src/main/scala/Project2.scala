30c61987b6cbf88b6ebb98cebbb49da202ddc7b8
Alvin Lee <a3171998@gmail.com>
2022-02-11 07:50:38 -0800

    added queries 4-8

--- src/main/scala/Project2.scala
+++ src/main/scala/Project2.scala
@@ -1,113 +1,102 @@
-import org.apache.spark.sql.hive.HiveContext
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.SparkConf
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.sql.SQLContext
-import java.sql.DriverManager
-import java.sql.Connection
-import java.util.Scanner
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.expressions.UserDefinedFunction
 
 object Project2 {
-  def main(args: Array[String]): Unit = {
-//added a comment
-    val spark = SparkSession.builder
-    .master("local[*]")
-    .appName("Project2")
-    .getOrCreate()
-
-    // This code is necessary for windows-based spark/hive/hadoop environments
-    System.setSecurityManager(null)
-    /*
-      System.setProperty(
-      "hadoop.home.dir",
-      "C:\\hadoop\\"
-    ) 
-    */ 
-
-    val conf = new SparkConf()
-      .setMaster("local")
-      .setAppName("Project2") // Change to whatever app name you want
-    val sc = new SparkContext(conf)
-    sc.setLogLevel("ERROR")
+  def main(args:Array[String]): Unit = {
+        val spark : SparkSession = SparkSession
+                    .builder
+                    .appName("SparkVSCode")
+                    .config("spark.master", "local")
+                    .getOrCreate()
 
     
-    /*
-     * Here is where we might take
-     * in an input from user
-     */
 
+    spark.sparkContext.setLogLevel("ERROR")
+    import spark.implicits._
 
-
-    queryOne()
-    queryTwo()
-    queryThree()
-    queryFour()
-    queryFive()
-    querySix()
-    querySeven()
-    queryEight()
+    // Feel free to rename these functions
+    queryOne(spark)
+    queryTwo(spark)
+    queryThree(spark)
+    queryFour(spark)
+    queryFive(spark)
+    querySix(spark)
+    querySeven(spark)
+    queryEight(spark)
     queryNine()
     queryTen()
 
 
-    sc.stop() // Necessary to close spark cleanly.
+    spark.stop() // Necessary to close spark cleanly.
   }
 
   
 
-  def insertCovidData(spark: SparkSession): Unit = {
-    val df = spark.read.csv("input/covid-data.csv")
-    val rddFromFile = spark.sparkContext.textFile("input/covid-data.csv") // Read CSV file
-    val rdd = rddFromFile.map(f=>{f.split(",")}) // Skip header
 
-    /*
-     * This statement creates a DataFrameReader from your file that you wish to pass in. 
-     * We can infer the schema and retrieve column names if the first row in your csv
-     * file has the column names. If not wanted, remove those options.
-     */ 
-     
+  def queryOne(spark: SparkSession): Unit = {
 
-    /*
-    val output = spark.read
-      .format("csv")
-      .option("inferSchema", "true")
-      .option("header", "true")
-      .load("input/covid-data.csv")
-    output.limit(15).show() // Prints out the first 15 lines of the dataframe
-    */
+    // Load in covid-data.csv
+    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
+
+    // Selects TOTAL CASES
+    df.select("location","total_cases").groupBy("location").agg(max("total_cases")).distinct().show()
   }
 
-  def queryOne(): Unit = {
-    
+  def queryTwo(spark: SparkSession): Unit = {
+
+    // Load in covid-data.csv
+    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
+
+    // Selects TOTAL DISTINCT CASES in 'Asia' 
+    df.select("location","total_cases").where(col("continent") === "Asia").groupBy("location").agg(max("total_cases")).distinct().show()
+   
   }
 
-  def queryTwo(): Unit = {
-    
+  def queryThree(spark: SparkSession): Unit = {
+    // Load in covid-data.csv
+    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
+
+    // Selects MAX cases in 'Asia' 
+    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).where(col("continent") === "Asia").show()
+
   }
 
-  def queryThree(): Unit = {
-    
+  def queryFour(spark: SparkSession): Unit = {
+    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
+
+    // Selects MAX cases in 'Africa' 
+    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).where(col("continent") === "Africa").show()
   }
 
-  def queryFour(): Unit = {
-    
+  def queryFive(spark: SparkSession): Unit = {
+    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
+
+    // Selects MAX cases in 'Europe' 
+    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).where(col("continent") === "Europe").show()
   }
 
-  def queryFive(): Unit = {
-    
+  def querySix(spark: SparkSession): Unit = {
+    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
+
+    // Selects MAX cases in 'North America' 
+    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).where(col("continent") === "North America").show()
   }
 
-  def querySix(): Unit = {
-    
+  def querySeven(spark: SparkSession): Unit = {
+    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
+
+    // Selects MAX cases in 'South America' 
+    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).where(col("continent") === "South America").show()
   }
 
-  def querySeven(): Unit = {
-    
-  }
+  def queryEight(spark: SparkSession): Unit = {
+    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
 
-  def queryEight(): Unit = {
-    
+    // Selects MAX cases in 'Oceania' 
+    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).where(col("continent") === "Oceania").show()
   }
 
   def queryNine(): Unit = {
