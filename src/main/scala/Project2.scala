fdf126233ada4b5f908f5b6bc2d692d8bc3a2151
Alvin Lee <a3171998@gmail.com>
2022-02-11 08:53:57 -0800

    update to match main

--- src/main/scala/Project2.scala
+++ src/main/scala/Project2.scala
@@ -17,6 +17,8 @@ object Project2 {
     spark.sparkContext.setLogLevel("ERROR")
     import spark.implicits._
 
+    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
+
     // Feel free to rename these functions
     queryOne(spark)
     queryTwo(spark)
@@ -31,82 +33,52 @@ object Project2 {
 
 
     spark.stop() // Necessary to close spark cleanly.
-  }
-
-  
-
-
   def queryOne(spark: SparkSession): Unit = {
-
-    // Load in covid-data.csv
-    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
-
     // Selects TOTAL CASES
     df.select("location","total_cases").groupBy("location").agg(max("total_cases")).distinct().show()
   }
 
   def queryTwo(spark: SparkSession): Unit = {
-
-    // Load in covid-data.csv
-    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
-
-    // Selects TOTAL DISTINCT CASES in 'Asia' 
-    df.select("location","total_cases").where(col("continent") === "Asia").groupBy("location").agg(max("total_cases")).distinct().show()
-   
+    // Selects MAX cases in 'Asia' 
+    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).show()
   }
 
   def queryThree(spark: SparkSession): Unit = {
-    // Load in covid-data.csv
-    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
-    // Selects MAX cases in 'Aisia' 
-    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).where(col("continent") === "Asia").show()
+    // Selects TOTAL DISTINCT CASES in 'Asia' 
+    df.select("location","total_cases").where(col("continent") === "Asia").groupBy("location").agg(max("total_cases")).distinct().show()
   }
 
   def queryFour(spark: SparkSession): Unit = {
-    // Load in covid-data.csv
-    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
-
     // Selects TOTAL DISTINCT CASES in 'Africa' 
     df.select("location","total_cases").where(col("continent") === "Africa").groupBy("location").agg(max("total_cases")).distinct().show()
   }
 
   def queryFive(spark: SparkSession): Unit = {
-    // Load in covid-data.csv
-    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
-
     // Selects TOTAL DISTINCT CASES in 'Europe' 
     df.select("location","total_cases").where(col("continent") === "Europe").groupBy("location").agg(max("total_cases")).distinct().show()
   }
 
   def querySix(spark: SparkSession): Unit = {
-    // Load in covid-data.csv
-    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
-
     // Selects TOTAL DISTINCT CASES in 'North America' 
     df.select("location","total_cases").where(col("continent") === "North America").groupBy("location").agg(max("total_cases")).distinct().show()
   }
 
   def querySeven(spark: SparkSession): Unit = {
-    // Load in covid-data.csv
-    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
-
     // Selects TOTAL DISTINCT CASES in 'South America' 
     df.select("location","total_cases").where(col("continent") === "South America").groupBy("location").agg(max("total_cases")).distinct().show()
   }
 
   def queryEight(spark: SparkSession): Unit = {
-    // Load in covid-data.csv
-    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
-
     // Selects TOTAL DISTINCT CASES in 'Oceania' 
     df.select("location","total_cases").where(col("continent") === "Oceania").groupBy("location").agg(max("total_cases")).distinct().show()
   }
 
   def queryNine(): Unit = {
-    
+
   }
 
   def queryTen(): Unit = {
-    
+
   }
+}
 }
