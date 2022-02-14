a3444d4f86fd9e5fa9572d5445a8ee6439534e55
creativezu <chris.nzuriwatu@gmail.com>
2022-02-14 14:24:02 -0500

    Added writing csv to output folder for each query q1 - q9

--- src/main/scala/Project2.scala
+++ src/main/scala/Project2.scala
@@ -5,19 +5,19 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.UserDefinedFunction
 
 object Project2 {
-  def main(args:Array[String]): Unit = {
-        val spark : SparkSession = SparkSession
-                    .builder
-                    .appName("SparkVSCode")
-                    .config("spark.master", "local")
-                    .getOrCreate()
-
-    
+  def main(args: Array[String]): Unit = {
+    val spark: SparkSession = SparkSession.builder
+      .appName("SparkVSCode")
+      .config("spark.master", "local")
+      .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
     import spark.implicits._
 
-    val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
+    val df = spark.read
+      .format("com.databricks.spark.csv")
+      .option("header", true)
+      .load("input/covid-data.csv")
 
     // Feel free to rename these functions
     queryOne(spark)
@@ -31,55 +31,216 @@ object Project2 {
     queryNine(spark)
     queryTen()
 
-
     spark.stop() // Necessary to close spark cleanly.
-  def queryOne(spark: SparkSession): Unit = {
-    // Selects TOTAL CASES
-    df.select("location","total_cases").groupBy("location").agg(max("total_cases")).distinct().show()
-  }
+    def queryOne(spark: SparkSession): Unit = {
+      // Selects TOTAL CASES
+      df.select("location", "total_cases")
+        .groupBy("location")
+        .agg(max("total_cases").alias("total_cases"))
+        .distinct()
+        .show()
 
-  def queryTwo(spark: SparkSession): Unit = {
-    // Selects MAX cases in 'Asia' 
-    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).show()
-  }
+      var q1 = df
+        .select("location", "total_cases")
+        .groupBy("location")
+        .agg(max("total_cases").alias("total_cases"))
+        .distinct()
 
-  def queryThree(spark: SparkSession): Unit = {
-    // Selects TOTAL DISTINCT CASES in 'Asia' 
-    df.select("location","total_cases").where(col("continent") === "Asia").groupBy("location").agg(max("total_cases")).distinct().show()
-  }
+      q1.coalesce(1)
+        .write
+        .format("com.databricks.spark.csv")
+        .mode("overwrite")
+        .option("header", "true")
+        .save("output/queryOne")
+    }
 
-  def queryFour(spark: SparkSession): Unit = {
-    // Selects TOTAL DISTINCT CASES in 'Africa' 
-    df.select("location","total_cases").where(col("continent") === "Africa").groupBy("location").agg(max("total_cases")).distinct().show()
-  }
+    def queryTwo(spark: SparkSession): Unit = {
+      // Selects MAX cases in 'Asia'
+      df.select("continent", "location", "total_cases")
+        .groupBy("continent")
+        .agg(max("total_cases"))
+        .show()
 
-  def queryFive(spark: SparkSession): Unit = {
-    // Selects TOTAL DISTINCT CASES in 'Europe' 
-    df.select("location","total_cases").where(col("continent") === "Europe").groupBy("location").agg(max("total_cases")).distinct().show()
-  }
+      val q2 = df
+        .select("continent", "location", "total_cases")
+        .groupBy("continent")
+        .agg(max("total_cases").alias("total_cases"))
 
-  def querySix(spark: SparkSession): Unit = {
-    // Selects TOTAL DISTINCT CASES in 'North America' 
-    df.select("location","total_cases").where(col("continent") === "North America").groupBy("location").agg(max("total_cases")).distinct().show()
-  }
+      q2.coalesce(1)
+        .write
+        .format("com.databricks.spark.csv")
+        .mode("overwrite")
+        .option("header", "true")
+        .save("output/queryTwo")
+    }
 
-  def querySeven(spark: SparkSession): Unit = {
-    // Selects TOTAL DISTINCT CASES in 'South America' 
-    df.select("location","total_cases").where(col("continent") === "South America").groupBy("location").agg(max("total_cases")).distinct().show()
-  }
+    def queryThree(spark: SparkSession): Unit = {
+      // Selects TOTAL DISTINCT CASES in 'Asia'
+      df.select("location", "total_cases")
+        .where(col("continent") === "Asia")
+        .groupBy("location")
+        .agg(max("total_cases").alias("total_cases"))
+        .distinct()
+        .show()
 
-  def queryEight(spark: SparkSession): Unit = {
-    // Selects TOTAL DISTINCT CASES in 'Oceania' 
-    df.select("location","total_cases").where(col("continent") === "Oceania").groupBy("location").agg(max("total_cases")).distinct().show()
-  }
+      val q3 = df
+        .select("location", "total_cases")
+        .where(col("continent") === "Asia")
+        .groupBy("location")
+        .agg(max("total_cases").alias("total_cases"))
+        .distinct()
 
-  def queryNine(spark: SparkSession): Unit = {
-    // Selects TOTAL DEATHS
-    df.select("location","total_deaths").groupBy("location").agg(max("total_deaths")).distinct().show()
-  }
+      q3.coalesce(1)
+        .write
+        .format("com.databricks.spark.csv")
+        .mode("overwrite")
+        .option("header", "true")
+        .save("output/queryThree")
+    }
 
-  def queryTen(): Unit = {
+    def queryFour(spark: SparkSession): Unit = {
+      // Selects TOTAL DISTINCT CASES in 'Africa'
+      df.select("location", "total_cases")
+        .where(col("continent") === "Africa")
+        .groupBy("location")
+        .agg(max("total_cases"))
+        .distinct()
+        .show()
 
+      val q4 = df
+        .select("location", "total_cases")
+        .where(col("continent") === "Africa")
+        .groupBy("location")
+        .agg(max("total_cases").alias("total_cases"))
+        .distinct()
+
+      q4.coalesce(1)
+        .write
+        .format("com.databricks.spark.csv")
+        .mode("overwrite")
+        .option("header", "true")
+        .save("output/queryFour")
+    }
+
+    def queryFive(spark: SparkSession): Unit = {
+      // Selects TOTAL DISTINCT CASES in 'Europe'
+      df.select("location", "total_cases")
+        .where(col("continent") === "Europe")
+        .groupBy("location")
+        .agg(max("total_cases"))
+        .distinct()
+        .show()
+
+      val q5 = df
+        .select("location", "total_cases")
+        .where(col("continent") === "Europe")
+        .groupBy("location")
+        .agg(max("total_cases").alias("total_cases"))
+        .distinct()
+
+      q5.coalesce(1)
+        .write
+        .format("com.databricks.spark.csv")
+        .mode("overwrite")
+        .option("header", "true")
+        .save("output/queryFive")
+    }
+
+    def querySix(spark: SparkSession): Unit = {
+      // Selects TOTAL DISTINCT CASES in 'North America'
+      df.select("location", "total_cases")
+        .where(col("continent") === "North America")
+        .groupBy("location")
+        .agg(max("total_cases"))
+        .distinct()
+        .show()
+
+      val q6 = df
+        .select("location", "total_cases")
+        .where(col("continent") === "North America")
+        .groupBy("location")
+        .agg(max("total_cases").alias("total_cases"))
+        .distinct()
+
+      q6.coalesce(1)
+        .write
+        .format("com.databricks.spark.csv")
+        .mode("overwrite")
+        .option("header", "true")
+        .save("output/querySix")
+    }
+
+    def querySeven(spark: SparkSession): Unit = {
+      // Selects TOTAL DISTINCT CASES in 'South America'
+      df.select("location", "total_cases")
+        .where(col("continent") === "South America")
+        .groupBy("location")
+        .agg(max("total_cases"))
+        .distinct()
+        .show()
+
+      val q7 = df
+        .select("location", "total_cases")
+        .where(col("continent") === "South America")
+        .groupBy("location")
+        .agg(max("total_cases").alias("total_cases"))
+        .distinct()
+
+      q7.coalesce(1)
+        .write
+        .format("com.databricks.spark.csv")
+        .mode("overwrite")
+        .option("header", "true")
+        .save("output/querySeven")
+
+    }
+
+    def queryEight(spark: SparkSession): Unit = {
+      // Selects TOTAL DISTINCT CASES in 'Oceania'
+      df.select("location", "total_cases")
+        .where(col("continent") === "Oceania")
+        .groupBy("location")
+        .agg(max("total_cases"))
+        .distinct()
+        .show()
+
+      val q8 = df
+        .select("location", "total_cases")
+        .where(col("continent") === "Oceania")
+        .groupBy("location")
+        .agg(max("total_cases").alias("total_cases"))
+        .distinct()
+
+      q8.coalesce(1)
+        .write
+        .format("com.databricks.spark.csv")
+        .mode("overwrite")
+        .option("header", "true")
+        .save("output/queryEight")
+    }
+
+    def queryNine(spark: SparkSession): Unit = {
+      // Selects TOTAL DEATHS
+      df.select("location", "total_deaths")
+        .groupBy("location")
+        .agg(max("total_deaths"))
+        .distinct()
+        .show()
+
+      val q9 = df
+        .select("location", "total_deaths")
+        .groupBy("location")
+        .agg(max("total_deaths").alias("total_deaths"))
+        .distinct()
+
+      q9.coalesce(1)
+        .write
+        .format("com.databricks.spark.csv")
+        .mode("overwrite")
+        .option("header", "true")
+        .save("output/queryNine")
+    }
+
+    def queryTen(): Unit = {}
   }
-}
 }
