70ba236a32442c2c4d780e6e7d86000a9c0e85b1
Alvin Lee <a3171998@gmail.com>
2022-02-15 12:39:40 -0800

    update: get rid of nulls

--- src/main/scala/Project2.scala
+++ src/main/scala/Project2.scala
@@ -20,62 +20,62 @@ object Project2 {
     val df = spark.read.format("com.databricks.spark.csv").option("header", true).load("input/covid-data.csv")
 
     // Feel free to rename these functions
-    // queryOne(spark)
-    // queryTwo(spark)
-    // queryThree(spark)
-    // queryFour(spark)
-    // queryFive(spark)
-    // querySix(spark)
-    // querySeven(spark)
-    // queryEight(spark)
-    // queryNine(spark)
+    queryOne(spark)
+    queryTwo(spark)
+    queryThree(spark)
+    queryFour(spark)
+    queryFive(spark)
+    querySix(spark)
+    querySeven(spark)
+    queryEight(spark)
+    queryNine(spark)
     queryTen(spark)
 
 
     spark.stop() // Necessary to close spark cleanly.
   def queryOne(spark: SparkSession): Unit = {
     // Selects TOTAL CASES
-    df.select("location","total_cases").groupBy("location").agg(max("total_cases")).distinct().show()
+    df.select("location","total_cases").groupBy("location").agg(max("total_cases")).distinct().na.drop("any").show(false)
   }
 
   def queryTwo(spark: SparkSession): Unit = {
     // Selects MAX cases in 'Asia' 
-    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).show()
+    df.select("continent","location","total_cases").groupBy("continent").agg(max("total_cases")).na.drop("any").show(false)
   }
 
   def queryThree(spark: SparkSession): Unit = {
     // Selects TOTAL DISTINCT CASES in 'Asia' 
-    df.select("location","total_cases").where(col("continent") === "Asia").groupBy("location").agg(max("total_cases")).distinct().show()
+    df.select("location","total_cases").where(col("continent") === "Asia").groupBy("location").agg(max("total_cases")).distinct().na.drop("any").show(false)
   }
 
   def queryFour(spark: SparkSession): Unit = {
     // Selects TOTAL DISTINCT CASES in 'Africa' 
-    df.select("location","total_cases").where(col("continent") === "Africa").groupBy("location").agg(max("total_cases")).distinct().show()
+    df.select("location","total_cases").where(col("continent") === "Africa").groupBy("location").agg(max("total_cases")).distinct().na.drop("any").show(false)
   }
 
   def queryFive(spark: SparkSession): Unit = {
     // Selects TOTAL DISTINCT CASES in 'Europe' 
-    df.select("location","total_cases").where(col("continent") === "Europe").groupBy("location").agg(max("total_cases")).distinct().show()
+    df.select("location","total_cases").where(col("continent") === "Europe").groupBy("location").agg(max("total_cases")).distinct().na.drop("any").show(false)
   }
 
   def querySix(spark: SparkSession): Unit = {
     // Selects TOTAL DISTINCT CASES in 'North America' 
-    df.select("location","total_cases").where(col("continent") === "North America").groupBy("location").agg(max("total_cases")).distinct().show()
+    df.select("location","total_cases").where(col("continent") === "North America").groupBy("location").agg(max("total_cases")).distinct().na.drop("any").show(false)
   }
 
   def querySeven(spark: SparkSession): Unit = {
     // Selects TOTAL DISTINCT CASES in 'South America' 
-    df.select("location","total_cases").where(col("continent") === "South America").groupBy("location").agg(max("total_cases")).distinct().show()
+    df.select("location","total_cases").where(col("continent") === "South America").groupBy("location").agg(max("total_cases")).distinct().na.drop("any").show(false)
   }
 
   def queryEight(spark: SparkSession): Unit = {
     // Selects TOTAL DISTINCT CASES in 'Oceania' 
-    df.select("location","total_cases").where(col("continent") === "Oceania").groupBy("location").agg(max("total_cases")).distinct().show()
+    df.select("location","total_cases").where(col("continent") === "Oceania").groupBy("location").agg(max("total_cases")).distinct().na.drop("any").show(false)
   }
 
   def queryNine(spark: SparkSession): Unit = {
     // Selects TOTAL DEATHS
-    df.select("location","total_deaths").groupBy("location").agg(max("total_deaths")).distinct().na.drop("any").show()
+    df.select("location","total_deaths").groupBy("location").agg(max("total_deaths")).distinct().na.drop("any").na.drop("any").show(false)
   }
 
   def queryTen(spark: SparkSession): Unit = {
